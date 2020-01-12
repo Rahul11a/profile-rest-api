bc4222b59b4cc98204181b772237075b36906840
Rahul11a <rahulsharma240301@gmail.com>
2020-01-12 15:09:56 +0530

    Added post put patch del method to our apiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-# When we call the APIView it returns a Standanrd respose object
+from rest_framework.response import Response # When we call the APIView it returns a Standanrd respose object
+from rest_framework import status #list of handy HTTP status codes that can be used when returning responses from api
+from profiles_api import serializers # Direct what to when post, put and patch requests are made
+# This comes from the serializers.py we made
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -18,3 +21,28 @@ class HelloApiView(APIView):
         # It returns a response object, which is further converted into json.
         # Hence it should either return dictionary or list
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, # List of all the errors on each validation
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating/replacing an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
