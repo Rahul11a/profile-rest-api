099e3cd5e982472436d45665cc6ff56b9e925ecc
Rahul11a <rahulsharma240301@gmail.com>
2020-02-12 00:19:11 +0530

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -28,8 +28,8 @@ DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 # Hence when we run on our local server DEBUG is True
 
 ALLOWED_HOSTS = [
-    'ec2-13-235-247-71.ap-south-1.compute.amazonaws.com'
-    '127.0.0.1'
+    'ec2-13-235-247-71.ap-south-1.compute.amazonaws.com',
+    '127.0.0.1',
 ]
 
 
